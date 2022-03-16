93842b305223095667abedc5327d26813b6e3b44
Enescan Akyüz <e_can70@hotmail.com>
2022-03-16 23:15:37 +0300

    Create Test.java

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+
+public class Test {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		Panel panel = new Panel();
+		panel.Giris();
+		Menu menu = new Menu();
+		menu.Menuler();
+		
+
+	}
+
+}
